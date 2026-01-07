72b6a3d52dff06b6737bca79d9e1956e981beb16
teke <tekeb98@gmail.com>
2026-01-08 00:22:41 +0900

    PageTransation added

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,18 +3,20 @@ import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
+import PageTransition from "@/components/PageTransition";
 
 export default function Page() {
   return (
     <>
       <Header />
-
-      <main className="container mx-auto">
-        <Hero />
-        <Skills />
-        <Experience />
-        <Projects />
-      </main>
+      <PageTransition>
+        <main className="container mx-auto space-y-32">
+          <Hero />
+          <Skills />
+          <Experience />
+          <Projects />
+        </main>
+      </PageTransition>
     </>
   );
 }
